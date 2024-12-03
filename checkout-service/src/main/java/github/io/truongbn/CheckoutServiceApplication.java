d7cee0af993745a8ab3ead2399a9eaa30f76be7a
Truong Bui <buingoctruong1508@gmail.com>
2024-12-04 06:58:16 +0900

    Add properties and dependencies for checkout service

---
+++ checkout-service/src/main/java/github/io/truongbn/CheckoutServiceApplication.java
@@ -0,0 +1,11 @@
+package github.io.truongbn;
+
+/**
+ * Hello world!
+ *
+ */
+public class CheckoutServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
