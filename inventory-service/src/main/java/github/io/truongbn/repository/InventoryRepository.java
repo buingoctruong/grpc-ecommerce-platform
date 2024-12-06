daa1385d8caa2d0451de7231b3bd9b70981c9054
Truong Bui <buingoctruong1508@gmail.com>
2024-12-06 11:16:42 +0900

    Update database updating command for inventory service

--- inventory-service/src/main/java/github/io/truongbn/repository/InventoryRepository.java
+++ inventory-service/src/main/java/github/io/truongbn/repository/InventoryRepository.java
@@ -3,7 +3,9 @@ package github.io.truongbn.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +16,8 @@ public interface InventoryRepository extends JpaRepository<Inventory, String> {
     Optional<Inventory> findByItemId(String itemId);
 
     @Transactional
-    @Query("UPDATE Inventory i SET i.quantities = :newQuantity WHERE i.id = :itemId")
-    int updateQuantitiesByItemId(String itemId, long newQuantity);
+    @Modifying
+    @Query("UPDATE Inventory i SET i.quantities = i.quantities - :purchaseQuantities WHERE i.itemId = :itemId")
+    int updateQuantitiesByItemId(@Param("itemId") String itemId,
+            @Param("purchaseQuantities") long purchaseQuantities);
 }
